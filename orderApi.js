0381a493e8606601bc481e02a748544b2471deb5
CJRMSolutions <r-dev-solutions@outlook.com>
2025-03-20 19:37:53 -0600

    Initial commit

--- orderApi.js
+++ orderApi.js
@@ -9,7 +9,14 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    writeConcern: {
+        w: 'majority',
+        j: true
+    }
+})
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB', err));
 
